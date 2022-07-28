ef894b26ad384d380bc43962a40999dc56610c78
凌 <nu11ptr@foxmail.com>
2022-07-28 21:12:00 +0800

    添加container自动重启功能

--- XesChroot.py
+++ XesChroot.py
@@ -101,10 +101,16 @@ def run():
         print("--- Stage 3/3 of Initalizing ---")
         print("---> Changing root directory to (container)/chroot/")
         print("---> Executing first-run script")
-        ret = os.system(
-            "LD_PRELOAD=$(pwd)/utils/container.so utils/busybox chroot chroot/ sh -c " + json.dumps(firstRunScript)
-        )
-        print("\n\n---> Program Exited with return code {}".format(ret))
+        ret = 0
+        while 1:
+            ret = os.system(
+                "LD_PRELOAD=$(pwd)/utils/container.so utils/busybox chroot chroot/ sh -c " + json.dumps(firstRunScript)
+            )
+            if ret==32512:
+                print("---> :( Killed, restarting docker")
+            else:
+                break
+        print("\n\n---> Program exited with return code {}".format(ret))
         # 第三阶段完成
     finally:
         # 回收资源
